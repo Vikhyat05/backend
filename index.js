06041242f08d930098fcc69baabeee475e1970f2
Lotus <vikhyatrocks@gmail.com>
2024-07-09 22:47:05 +0530

    Github Data Added

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const app = express()
 
 const port = 3000
 
+const githubData = {
+  "login": "Vikhyat05",
+  "id": 44644078,
+  "node_id": "MDQ6VXNlcjQ0NjQ0MDc4",
+  "avatar_url": "https://avatars.githubusercontent.com/u/44644078?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Vikhyat05",
+  "html_url": "https://github.com/Vikhyat05",
+  "followers_url": "https://api.github.com/users/Vikhyat05/followers",
+  "following_url": "https://api.github.com/users/Vikhyat05/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Vikhyat05/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Vikhyat05/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Vikhyat05/subscriptions",
+  "organizations_url": "https://api.github.com/users/Vikhyat05/orgs",
+  "repos_url": "https://api.github.com/users/Vikhyat05/repos",
+  "events_url": "https://api.github.com/users/Vikhyat05/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Vikhyat05/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Lotus",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 5,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2018-10-31T13:57:43Z",
+  "updated_at": "2024-06-02T05:12:40Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -18,6 +53,10 @@ app.get('/login',(req,res)=>{
 
 app.get('/continuOS',(req,res)=>{
   res.send('<h2>Work in Progress</h2>')
+})
+
+app.get('/github',(req,res)=>{
+  res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
