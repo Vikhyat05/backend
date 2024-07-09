515b6d79c1abc74fb985422dfae9b47cefa9fb4c
vikhyat rana <vikhyatrana@MacBook-Pro.local>
2024-07-09 21:50:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')// same thing as import express from "express"
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+  res.send('vikhyatRana')
+})
+
+app.get('/login',(req,res)=>{
+  res.send('<h1>Please login for access</h1>')
+})
+
+app.get('/continuOS',(req,res)=>{
+  res.send('<h2>Work in Progress</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+console.log(process.env.PORT)
